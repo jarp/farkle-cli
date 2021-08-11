7545f4d881f7c7b4d7dc80c87e3700d1ac58f48f
Jonathan Arp <jarp@nd.edu>
2021-08-11 17:35:49 -0400

    added app with intro

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+import sys
+os.system("clear")
+
+print("\n\n\n\n")
+
+print('FARKLE!!!')
+print("##############################################################\n")
+print("Farkle as a CLI. What fun!\nCoded by Sir Captain Dr. Professor\n")
+print("##############################################################\n\n")
