c3ce360ec806228d17c5857e7c310714bdd30814
Jonathan Arp <jarp@nd.edu>
2021-08-12 09:33:09 -0400

    added a bit of loopy stuff

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from datetime import date
 os.system("clear")
 
 print("\n\n\n\n")
@@ -8,3 +9,14 @@ print('FARKLE!!!')
 print("##############################################################\n")
 print("Farkle as a CLI. What fun!\nCoded by Sir Captain Dr. Professor\n")
 print("##############################################################\n\n")
+
+players = []
+for i in range(2):
+    player = input("Enter player's name: ")
+    players.append(player)
+
+print("There are {} players. There names are {}".format(len(players), ", ".join(players)))
+
+game = {'created_at': date.today()}
+game['players'] = players
+print(game)
