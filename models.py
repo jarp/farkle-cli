1608bf447db4e76a21a98fbc887d9af2c35949aa
Jonathan Arp <jarp@nd.edu>
2021-08-12 10:15:05 -0400

    added two different dice types

--- models.py
+++ models.py
@@ -33,3 +33,22 @@ class Dice:
 
   def role(self):
     raise "you must implement this method"
+
+## Specific Dice that contains basic sequential numbers
+class NumericDice(Dice):
+  def __init__(self, number=6):
+    self.number = number
+
+  def role(self):
+    # print("role a random number bt 1 and ", self.number)
+    return random.randint(1, self.number)
+
+## Specific Dice that can be inited with a list of values (ints/strings) Think DnD or Lucky 8 Ball
+class ListDice(Dice):
+  def __init__(self, values=[]):
+    self.values = values
+
+  def role(self):
+    random_index = random.randint(0, (len(self.values) -1) )
+    # print("get value between 0 and {} which is {}".format( (len(self.values) -1), random_index))
+    return self.values[random_index]
